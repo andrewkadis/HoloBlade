f94d691512e6674773aa0674e5314e029b268e05
Kadds <ack49@cam.ac.uk>
2019-08-06 10:44:57 +0100

    Initial commit of HoloBlade firmware

---
+++ scripts/build.sh
@@ -0,0 +1,28 @@
+# A. Kadis 6th August 2019
+# This is a script to call the iCECube2 tools from the command-line
+# Tested using gitbash/mingw from within VSCode
+# Information came from the iCEcube2 User Guide - iCEcube201708UserGuide.pdf
+# Make sure you get the latest because key info is not in older versions!!!
+
+# Execution Directory - should be from the project root directory 
+EXEC_DIR=~/Documents/Dev/awesome-board-firmware
+cd $EXEC_DIR
+pwd
+
+# Setup Environment Variables
+export ICE_ROOT=/c/lscc/iCEcube2
+export LD_LIBRARY_PATH=$ICE_ROOT/sbt_backend/bin/win32/opt/synpwrap:$LD_LIBRARY_PATH
+export SYNPLIFY_PATH=$ICE_ROOT/synpbase
+export SBT_DIR=$ICE_ROOT/sbt_backend/
+
+# Run Synthesis Tool - in this case we use Synplify Pro as it is the default
+# See 'Holoblade_syn.prj' for synthesis options
+# Note that this file should not be changed manually as it will autooverwrite, should be edited from within iCEcube2 GUI
+$ICE_ROOT/sbt_backend/bin/win32/opt/synpwrap/synpwrap -prj holoblade_syn.prj -log icelog.log
+
+# Run Backend Tools
+# Note that configuration here is controlled by 'run_backend_tools.tcl'
+# Edit it to adjust 'backend' options
+# As of 6th August, all we care about is pin routing
+tclsh "./scripts/run_backend_tools.tcl"
+
---
+++ src/top.v
@@ -0,0 +1,88 @@
+/* 
+* Top module for HoloBlade Board
+*/
+
+module top(input CLK, 
+	output DEBUG_0,
+	output DEBUG_1,
+	output DEBUG_2,
+	output DEBUG_3,
+	output DEBUG_4
+);
+
+
+assign DEBUG_0 = 1;
+assign DEBUG_1 = 0;//CLK;
+//assign DEBUG_2 = 0;
+assign DEBUG_3 = sysclk_unbuf;
+assign DEBUG_4 = sysclk;
+
+wire sysclk_unbuf;
+wire sysclk;
+
+// Drive our system of a 50 MHz Clock
+// Note that due to PLL limitations, its actually 50.25HMz
+SB_PLL40_CORE #(.FEEDBACK_PATH("SIMPLE"),
+	   .PLLOUT_SELECT("GENCLK"),
+                .DIVR(4'b0001),
+                .DIVF(7'b1000010),
+                .DIVQ(3'b100),
+                .FILTER_RANGE(3'b001)
+) pll_config (
+                .REFERENCECLK(CLK),
+                .PLLOUTGLOBAL(sysclk_unbuf),
+                .LOCK(),
+                .RESETB(1'b1),
+                .BYPASS(1'b0)
+);
+
+// Buffer the output so it doesn't sag
+SB_GB clk_gb ( .USER_SIGNAL_TO_GLOBAL_BUFFER(sysclk_unbuf), .GLOBAL_BUFFER_OUTPUT(sysclk) );
+
+
+
+
+//reg temp_debug2;
+//always @ (posedge sysclk) begin
+//  	temp_debug2 = sysclk;
+//end
+//assign DEBUG_3 = temp_debug2;
+
+
+//extclk_pll extclk_pll_inst(.PACKAGEPIN(CLK),
+//                           .PLLOUTCORE(),
+//                           .PLLOUTGLOBAL(sysclk),
+//                           .RESET());
+
+
+//SB_GB_IO My_Clock_Buffer_Package_Pin ( // A users external Clock reference pin
+
+//	.PACKAGE_PIN (CLK), // User’s Pin signal name
+//	.LATCH_INPUT_VALUE (), // Latches/holds the Input value
+//	.CLOCK_ENABLE (), // Clock Enable common to input and output clock
+//	.INPUT_CLK (), // Clock for the input registers
+//	.OUTPUT_CLK (), // Clock for the output registers
+//	.OUTPUT_ENABLE (), // Output Pin Tristate/Enable // control
+//	.D_OUT_0 (), // Data 0 – out to Pin/Rising clk
+	
+	// edge
+//	.D_OUT_1 (), // Data 1 - out to Pin/Falling clk // edge
+//	.D_IN_0 (), // Data 0 - Pin input/Rising clk
+
+	// edge
+//	.D_IN_1 (), // Data 1 – Pin input/Falling clk // edge
+//	.GLOBAL_BUFFER_OUTPUT (sysclk)
+// Example use – clock buffer //driven from the input pin
+//);
+
+// defparam My_Clock_Buffer_Package_Pin.PIN_TYPE = 6'b000000;
+// See Input and Output Pin Function Tables.
+// Default value of PIN_TYPE = 6’000000 i.e.
+// an input pad, with the input signal
+// registered
+// Note that this primitive is a superset of the SB_IO primitive, and includes the connectivity to drive a Global Buffer. For example SB_GB_IO pins are likely to be used for external Clocks.
+
+
+
+
+endmodule 
