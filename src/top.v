51d67eea7f002288cdf1b3513a28c2cda3f43e69
Kadds <ack49@cam.ac.uk>
2019-08-06 11:59:12 +0100

    Got the UART RX being routed through the FPGA fabric

--- src/top.v
+++ src/top.v
@@ -2,7 +2,9 @@
 * Top module for HoloBlade Board
 */
 
-module top(input CLK, 
+module top(
+    input CLK, 
+    input UART_RX,
 	output DEBUG_0,
 	output DEBUG_1,
 	output DEBUG_2,
@@ -13,14 +15,14 @@ module top(input CLK,
 
 assign DEBUG_0 = 1;
 assign DEBUG_1 = 0;//CLK;
-//assign DEBUG_2 = 0;
-assign DEBUG_3 = sysclk_unbuf;
+assign DEBUG_2 = 1;
+// assign DEBUG_3 = sysclk_unbuf;
 assign DEBUG_4 = sysclk;
 
 wire sysclk_unbuf;
 wire sysclk;
 
-// Drive our system of a 50 MHz Clock
+// Drive our system off a 50 MHz Clock
 // Note that due to PLL limitations, its actually 50.25HMz
 SB_PLL40_CORE #(.FEEDBACK_PATH("SIMPLE"),
 	   .PLLOUT_SELECT("GENCLK"),
@@ -40,7 +42,8 @@ SB_PLL40_CORE #(.FEEDBACK_PATH("SIMPLE"),
 SB_GB clk_gb ( .USER_SIGNAL_TO_GLOBAL_BUFFER(sysclk_unbuf), .GLOBAL_BUFFER_OUTPUT(sysclk) );
 
 
-
+// Route the Uart Rx out of the chip
+assign DEBUG_3 = UART_RX;
 
 //reg temp_debug2;
 //always @ (posedge sysclk) begin
@@ -57,27 +60,27 @@ SB_GB clk_gb ( .USER_SIGNAL_TO_GLOBAL_BUFFER(sysclk_unbuf), .GLOBAL_BUFFER_OUTPU
 
 //SB_GB_IO My_Clock_Buffer_Package_Pin ( // A users external Clock reference pin
 
-//	.PACKAGE_PIN (CLK), // User’s Pin signal name
+//	.PACKAGE_PIN (CLK), // Userï¿½s Pin signal name
 //	.LATCH_INPUT_VALUE (), // Latches/holds the Input value
 //	.CLOCK_ENABLE (), // Clock Enable common to input and output clock
 //	.INPUT_CLK (), // Clock for the input registers
 //	.OUTPUT_CLK (), // Clock for the output registers
 //	.OUTPUT_ENABLE (), // Output Pin Tristate/Enable // control
-//	.D_OUT_0 (), // Data 0 – out to Pin/Rising clk
+//	.D_OUT_0 (), // Data 0 ï¿½ out to Pin/Rising clk
 	
 	// edge
 //	.D_OUT_1 (), // Data 1 - out to Pin/Falling clk // edge
 //	.D_IN_0 (), // Data 0 - Pin input/Rising clk
 
 	// edge
-//	.D_IN_1 (), // Data 1 – Pin input/Falling clk // edge
+//	.D_IN_1 (), // Data 1 ï¿½ Pin input/Falling clk // edge
 //	.GLOBAL_BUFFER_OUTPUT (sysclk)
-// Example use – clock buffer //driven from the input pin
+// Example use ï¿½ clock buffer //driven from the input pin
 //);
 
 // defparam My_Clock_Buffer_Package_Pin.PIN_TYPE = 6'b000000;
 // See Input and Output Pin Function Tables.
-// Default value of PIN_TYPE = 6’000000 i.e.
+// Default value of PIN_TYPE = 6ï¿½000000 i.e.
 // an input pad, with the input signal
 // registered
 // Note that this primitive is a superset of the SB_IO primitive, and includes the connectivity to drive a Global Buffer. For example SB_GB_IO pins are likely to be used for external Clocks.
