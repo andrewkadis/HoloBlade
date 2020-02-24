f76fb5a3a29f2990638de5492c9bb79b108fe3b8
Kadds <ack49@cam.ac.uk>
2020-02-24 15:39:30 +0000

    added more useful bits to early simulation stuff

--- myhdl_dev/src/my_hdl/bluejay_data.py
+++ myhdl_dev/src/my_hdl/bluejay_data.py
@@ -52,7 +52,6 @@ def bluejay_data(data_i, data_rdy_i, clk_i, data_o, sync_o):
     #             complete_tick.next = 1
     #         else :
     #             complete_tick.next = 0
-
     # # optional count
     # if count is not None:
     #     @always_comb
@@ -67,21 +66,40 @@ def bluejay_data(data_i, data_rdy_i, clk_i, data_o, sync_o):
 def bluejay_data_tb():
 
     # Signals
-    data_i = Signal(0)
+    data_i = Signal((intbv(0)[8:]))
     data_rdy_i = Signal(0)
-    clk_i = Signal(0)
-    data_o = Signal(0)
+    clk_i = Signal(intbv(0))
+    data_o = Signal((intbv(0)[8:]))
     sync_o = Signal(0)
 
     # Inst for testing
     bluejay_data_inst = bluejay_data(data_i, data_rdy_i, clk_i, data_o, sync_o)
 
     # Clock
-    @always(delay(period))
+    PERIOD = 20 # 50 MHz
+    @always(delay(PERIOD))
     def clkgen():
         clk_i.next = not clk_i
 
-    return bluejay_data_inst, clkgen
+    # Timing Code, useful for clearing our Assert signals
+    @always(delay(10))
+    def timing():
+        data_i.next = data_i.next + 1
+        # Clear Assert signals
+        if data_rdy_i==1:
+            data_rdy_i.next = 0    
+
+    # Data Ready goes high for 1 cycle for latching output
+    @always(delay(100))
+    def data_rdy_assert():
+        data_rdy_i.next = 1
+    # Clear next cycle
+    # @instance
+    # def data_rdy_clear():
+    #     while True:
+
+
+    return bluejay_data_inst, clkgen, data_rdy_assert, timing
 
 
 # Simulation
@@ -89,7 +107,7 @@ def simulate(timesteps):
 
     tb = bluejay_data_tb()
     tb.config_sim(trace=True)
-    tb.run_sim(timesteps)
+    tb.run_sim(1000)
 
 
 # Execution
