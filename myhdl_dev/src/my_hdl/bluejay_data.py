16d56e35c0f7f0e37fbedb6603a26dc33cdc7da5
Kadds <ack49@cam.ac.uk>
2020-02-24 16:18:38 +0000

    Fixed up the clock so its working

--- myhdl_dev/src/my_hdl/bluejay_data.py
+++ myhdl_dev/src/my_hdl/bluejay_data.py
@@ -5,15 +5,19 @@ from myhdl import *
 period = 20 # clk frequency = 50 MHz
 
 @block
-def bluejay_data(data_i, data_rdy_i, clk_i, data_o, sync_o):
+def bluejay_data(clk_i, data_i, data_rdy_i, get_next_word_o, data_o, sync_o):
 
     """ Peripheral to clock data out to a Bluejay SLM's Data Interface
 
     I/O pins:
     --------
-    clk_i       : 50MHz input clock
-    data_i      : 32-bit input data to be shown on SLM
-    data_rdy_i  : line to indicate that new data is available, active-high for 1 cycle
+    Control:
+    clk_i            : 50MHz input clock
+    Read-Side:
+    data_i           : 32-bit input data to be shown on SLM
+    data_rdy_i       : line to indicate that new data is available, active-high for 1 cycle
+    get_next_word_o  : line to pull next data word out of fifo 
+    Write-Side:
     data_o      : 32-bit output line to data interface on Bluejay SLM
     sync_o      : Synchronisation line on Bluejay SLM
 
@@ -27,89 +31,78 @@ def bluejay_data(data_i, data_rdy_i, clk_i, data_o, sync_o):
 
     return update
 
-    # count_reg = Signal(intbv(0)[N:0])
-    # count_next = Signal(intbv(0)[N:0])
-
-    # @always(clk.posedge, reset_n.negedge)
-    # def logic_reg():
-    #     if reset_n == 0 :
-    #         count_reg.next = 0
-    #     else :
-    #         count_reg.next = count_next
-
-    # @always_comb
-    # def logic_next():
-    #     if count_reg == M-1 :
-    #         count_next.next = 0
-    #     else :
-    #         count_next.next = count_reg + 1
-
-
-    # # optional complete_tick
-    # if complete_tick is not None:
-    #     @always_comb
-    #     def complete_tick_logic():
-    #         if count_reg == M-1 :
-    #             complete_tick.next = 1
-    #         else :
-    #             complete_tick.next = 0
-    # # optional count
-    # if count is not None:
-    #     @always_comb
-    #     def out_val():
-    #         count.next = count_reg
-
-    # return instances()  # return all instances
-
-
 # testbench
 @block
 def bluejay_data_tb():
 
     # Signals
+    # Control
+    clk_i = Signal(bool(0))
+    # Read-Side
     data_i = Signal((intbv(0)[8:]))
     data_rdy_i = Signal(False)
-    clk_i = Signal(intbv(0))
+    get_next_word_o = Signal(False)
+    # Write-Side
     data_o = Signal((intbv(0)[8:]))
     sync_o = Signal(0)
 
     # Inst for testing
-    bluejay_data_inst = bluejay_data(data_i, data_rdy_i, clk_i, data_o, sync_o)
+    bluejay_data_inst = bluejay_data(clk_i, data_i, data_rdy_i, get_next_word_o, data_o, sync_o)
 
     # Clock
-    PERIOD = 20 # 50 MHz
+    PERIOD = 10 # 50 MHz
     @always(delay(PERIOD))
     def clkgen():
         clk_i.next = not clk_i
 
     # Timing Code, useful for clearing our Assert signals
-    @always(delay(10))
+    @always(clk_i.posedge)
     def timing():
-        data_i.next = data_i.next + 1
+        # data_i.next = data_i.next + 1
         # Clear Assert signals
-        if data_rdy_i==1:
-            data_rdy_i.next = 0    
+        if data_rdy_i==True:
+            data_rdy_i.next = False
 
-    # Data Ready goes high for 1 cycle for latching output
-    @always(delay(100))
-    def data_rdy_assert():
-        data_rdy_i.next = 1
+    # # Data Ready goes high for 1 cycle for latching output
+    # @always(delay(100))
+    # def data_rdy_assert():
+    #     data_rdy_i.next = 1
     # Clear next cycle
     # @instance
     # def data_rdy_clear():
     #     while True:
 
+    # Load test data
+    @instance
+    def load_test_data():
+        while True:
+            yield delay(80)
+            data_i.next = intbv(10)[32:]
+            data_rdy_i.next = True
+            yield delay(100) 
+            data_i.next = intbv(20)[32:]
+            data_rdy_i.next = True
+            yield delay(100)
+            data_i.next = intbv(30)[32:]
+            data_rdy_i.next = True
+            yield delay(100)
+            data_i.next = intbv(40)[32:]
+            data_rdy_i.next = True
+            yield delay(100)
+            data_i.next = intbv(50)[32:]
+            data_rdy_i.next = True
 
-    return bluejay_data_inst, clkgen, data_rdy_assert, timing
+
+    return bluejay_data_inst, clkgen, timing, load_test_data
 
 
 # Simulation
-def simulate(timesteps):
+def simulate():
 
     tb = bluejay_data_tb()
     tb.config_sim(trace=True)
-    tb.run_sim(1000)
+    tb.run_sim(500)
 
 
 # Execution
-simulate(2000)
+simulate()
