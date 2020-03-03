84e8be0e9f24323e461993f8bf4db8842e63848d
Kadds <ack49@cam.ac.uk>
2020-03-03 21:11:23 +0000

    Added outputs generated from myHDL

--- src/top.v
+++ src/top.v
@@ -72,44 +72,44 @@ module top(
     output FT_SIWU,
     input  FR_RXF,
     input  FT_TXE,
-    output FIFO_BE3,
-    output FIFO_BE2,
-    output FIFO_BE1,
-    output FIFO_BE0,
-    output FIFO_D31,
-    output FIFO_D30,
-    output FIFO_D29,
-    output FIFO_D28,
-    output FIFO_D27,
+    input  FIFO_BE3,
+    input  FIFO_BE2,
+    input  FIFO_BE1,
+    input  FIFO_BE0,
+    input  FIFO_D31,
+    input  FIFO_D30,
+    input  FIFO_D29,
+    input  FIFO_D28,
+    input  FIFO_D27,
     input  FIFO_CLK,
-    output FIFO_D26,
-    output FIFO_D25,
-    output FIFO_D24,
-    output FIFO_D23,
-    output FIFO_D22,
-    output FIFO_D21,
-    output FIFO_D20,
-    output FIFO_D19,
-    output FIFO_D18,
-    output FIFO_D17,
-    output FIFO_D16,
+    input  FIFO_D26,
+    input  FIFO_D25,
+    input  FIFO_D24,
+    input  FIFO_D23,
+    input  FIFO_D22,
+    input  FIFO_D21,
+    input  FIFO_D20,
+    input  FIFO_D19,
+    input  FIFO_D18,
+    input  FIFO_D17,
+    input  FIFO_D16,
     // Bank 2 Pins
-    output FIFO_D15,
-    output FIFO_D14,
-    output FIFO_D13,
-    output FIFO_D12,
-    output FIFO_D11,
-    output FIFO_D10,
-    output FIFO_D9,
-    output FIFO_D8,
-    output FIFO_D7,
-    output FIFO_D6,
-    output FIFO_D5,
-    output FIFO_D4,
-    output FIFO_D3,
-    output FIFO_D2,
-    output FIFO_D1,
-    input  FIFO_D0,
+    input FIFO_D15,
+    input FIFO_D14,
+    input FIFO_D13,
+    input FIFO_D12,
+    input FIFO_D11,
+    input FIFO_D10,
+    input FIFO_D9,
+    input FIFO_D8,
+    input FIFO_D7,
+    input FIFO_D6,
+    input FIFO_D5,
+    input FIFO_D4,
+    input FIFO_D3,
+    input FIFO_D2,
+    input FIFO_D1,
+    input FIFO_D0,
 
     // Debug
     output DEBUG_0,
@@ -170,8 +170,8 @@ wire debug_ch2;
 wire debug_ch3;
 wire debug_ch4;
 // Map to scope probes
-assign DEBUG_9 = debug_ch1; // Goes to TP9
-assign DEBUG_8 = debug_ch2; // Goes to TP8
+// assign DEBUG_9 = debug_ch1; // Goes to TP9
+// assign DEBUG_8 = debug_ch2; // Goes to TP8
 assign DEBUG_5 = debug_ch3; // Goes to p5
 assign DEBUG_6 = debug_ch4; // Goes to p6
 // GPIOs attached to LEDs
@@ -273,34 +273,13 @@ assign reset_all_w = reset_all_r;
 
 // assign debug_ch1 = SEN;
 // assign debug_ch2 = SDAT;
-assign debug_ch3 = SOUT;
-assign debug_ch4 = SCK;
+// assign debug_ch3 = SOUT;
+// assign debug_ch4 = SCK;
 
-////////////////////////
-///////// USB3 /////////
-////////////////////////
-assign debug_ch1 = FR_RXF;
-assign debug_ch2 = FIFO_D0;
-reg FT_OE_r;
-reg FT_RD_r;
-assign FT_OE = FT_OE_r;
-assign FT_RD = FT_RD_r;
 
-always @(posedge FIFO_CLK) begin
 
-  // Default
-  FT_OE_r = 1;
-  FT_RD_r = 1;
 
-  // Trigger OE_N one cycle after RXF_N
-  if (FR_RXF==0)
-    FT_OE_r <= 0;
 
-  // Trigger RD_N one cycle after OE_N
-  if (FT_OE==0)
-    FT_RD_r <= 0;
-    
-  end
 
 
 
@@ -319,40 +298,203 @@ assign INVERT = 1'bx;
 assign SYNC   = 1'bx;
 assign VALID  = 1'bx;
 // Data Lines
-assign DATA31 = 1'b1;
-assign DATA0  = 1'b1;
-assign DATA30 = 1'b1;
-assign DATA29 = 1'b1;
-assign DATA1  = 1'b1;
-assign DATA28 = 1'b1;
-assign DATA27 = 1'b1;
-assign DATA2  = 1'b1;
-assign DATA26 = 1'b1;
-assign DATA25 = 1'b1;
-assign DATA3  = 1'b1;
-assign DATA24 = 1'b1;
-assign DATA23 = 1'b1;
-assign DATA4  = 1'b1;
-assign DATA22 = 1'b1;
-assign DATA21 = 1'b1;
-assign DATA5  = 1'b1;
-assign DATA20 = 1'b1;
-assign DATA19 = 1'b1;
-assign DATA6  = 1'b1;
-assign DATA18 = 1'b1;
-assign DATA17 = 1'b1;
-assign DATA7  = 1'b1;
-assign DATA16 = 1'b1;
-assign DATA15 = 1'b1;
-assign DATA8  = 1'b1;
-assign DATA14 = 1'b1;
-assign DATA13 = 1'b1;
-assign DATA12 = 1'b1;
-assign DATA11 = 1'b1;
-assign DATA9  = 1'b1;
-assign DATA10 = 1'b1;
+// assign DATA31 = 1'b1;
+// assign DATA0  = 1'b1;
+// assign DATA30 = 1'b1;
+// assign DATA29 = 1'b1;
+// assign DATA1  = 1'b1;
+// assign DATA28 = 1'b1;
+// assign DATA27 = 1'b1;
+// assign DATA2  = 1'b1;
+// assign DATA26 = 1'b1;
+// assign DATA25 = 1'b1;
+// assign DATA3  = 1'b1;
+// assign DATA24 = 1'b1;
+// assign DATA23 = 1'b1;
+// assign DATA4  = 1'b1;
+// assign DATA22 = 1'b1;
+// assign DATA21 = 1'b1;
+// assign DATA5  = 1'b1;
+// assign DATA20 = 1'b1;
+// assign DATA19 = 1'b1;
+// assign DATA6  = 1'b1;
+// assign DATA18 = 1'b1;
+// assign DATA17 = 1'b1;
+// assign DATA7  = 1'b1;
+// assign DATA16 = 1'b1;
+// assign DATA15 = 1'b1;
+// assign DATA8  = 1'b1;
+// assign DATA14 = 1'b1;
+// assign DATA13 = 1'b1;
+// assign DATA12 = 1'b1;
+// assign DATA11 = 1'b1;
+// assign DATA9  = 1'b1;
+// assign DATA10 = 1'b1;
+
+/////////////////////////////////////////
+//////// Bluejay Data Interface /////////
+/////////////////////////////////////////
+
+// Use GPIOs from USB3 Chip to drive next line andd next frame ready signals
+wire next_line_rdy_o_w;
+wire next_frame_rdy_o_w;
+// 32-Bit Fifo Data connection comes from USB-FIFO chip
+wire[31:0] usb_data_o;
+// Signals so that the bluejay_data FSM can manage data from USB-FIFO chip
+wire fifo_empty_i;
+wire get_next_word_o;
+// Map Bluejay Data Lines Out
+wire[31:0] bluejay_data_out;
+bluejay_data_out[31] = DATA31;
+bluejay_data_out[30] = DATA30;
+bluejay_data_out[29] = DATA29;
+bluejay_data_out[28] = DATA28;
+bluejay_data_out[27] = DATA27;
+bluejay_data_out[26] = DATA26;
+bluejay_data_out[25] = DATA25;
+bluejay_data_out[24] = DATA24;
+bluejay_data_out[23] = DATA23;
+bluejay_data_out[22] = DATA22;
+bluejay_data_out[21] = DATA21;
+bluejay_data_out[20] = DATA20;
+bluejay_data_out[19] = DATA19;
+bluejay_data_out[18] = DATA18;
+bluejay_data_out[17] = DATA17;
+bluejay_data_out[16] = DATA16;
+bluejay_data_out[15] = DATA15;
+bluejay_data_out[14] = DATA14;
+bluejay_data_out[13] = DATA13;
+bluejay_data_out[12] = DATA12;
+bluejay_data_out[11] = DATA11;
+bluejay_data_out[10] = DATA10;
+bluejay_data_out[19] = DATA19;
+bluejay_data_out[18] = DATA18;
+bluejay_data_out[17] = DATA17;
+bluejay_data_out[16] = DATA16;
+bluejay_data_out[15] = DATA15;
+bluejay_data_out[14] = DATA14;
+bluejay_data_out[13] = DATA13;
+bluejay_data_out[12] = DATA12;
+bluejay_data_out[11] = DATA11;
+bluejay_data_out[9]  = DATA9;
+bluejay_data_out[8]  = DATA8;
+bluejay_data_out[7]  = DATA7;
+bluejay_data_out[6]  = DATA6;
+bluejay_data_out[5]  = DATA5;
+bluejay_data_out[4]  = DATA4;
+bluejay_data_out[3]  = DATA3;
+bluejay_data_out[2]  = DATA2;
+bluejay_data_out[1]  = DATA1;
+bluejay_data_out[0]  = DATA0;
+
+// Instantiate Bluejay Data Interface
+bluejay_data_inst bluejay_data (
+
+  // Control
+  .clk_i(sys_clk),  //TODO: Fix our sysclk as this will be wrong
+  .reset_i(reset_all_w),
+  new_frame_i(next_frame_rdy_w),
+  // Read-Side:
+  .data_i(usb_data_o),
+  .next_line_rdy_i(next_line_rdy_w),
+  .fifo_empty_i(fifo_empty_i),
+  .get_next_word_o(get_next_word_o),
+  // Write-Side:
+  .data_o(bluejay_data_out),
+  .sync_o(SYNC),
+  .valid_o(VALID),
+  .update_o(UPDATE),
+  .invert_o(INVERT)
+);
 
 
+
+
+
+
+
+
+
+
+////////////////////////
+///////// USB3 /////////
+////////////////////////
+assign debug_ch1 = FT_OE;//FIFO_CLK;
+assign debug_ch2 = FR_RXF; //FIFO_D0
+assign debug_ch3 = FT_RD;
+assign debug_ch4 = FIFO_D0;
+// Connect all of our internal names up with names from schematic using wires
+wire RX_F;
+wire OE_N;
+wire RD_N;
+wire RESET_N;
+assign RX_F    = FR_RXF;
+assign OE_N    = FT_OE;
+assign RD_N    = FT_RD;
+assign RESET_N = = 1'bz;  //TODO: Would be great to connect this line in a future spin on the board
+// We get when the next line and frame are ready from USB GPIO 0 and 1
+// These are wired through TP8 and TP9 as not directly connected to FPGA
+wire next_line_rdy_i_w;
+wire next_frame_rdy_i_w;
+assign next_line_rdy_i_w  = DEBUG_8;
+assign next_frame_rdy_i_w = DEBUG_9;
+// Wire up our 32-bit data connection
+assign usb_data_o[31] = FIFO_D31;
+assign usb_data_o[30] = FIFO_D30;
+assign usb_data_o[29] = FIFO_D29;
+assign usb_data_o[28] = FIFO_D28;
+assign usb_data_o[27] = FIFO_D27;
+assign usb_data_o[26] = FIFO_D26;
+assign usb_data_o[25] = FIFO_D25;
+assign usb_data_o[24] = FIFO_D24;
+assign usb_data_o[23] = FIFO_D23;
+assign usb_data_o[22] = FIFO_D22;
+assign usb_data_o[21] = FIFO_D21;
+assign usb_data_o[20] = FIFO_D20;
+assign usb_data_o[19] = FIFO_D19;
+assign usb_data_o[18] = FIFO_D18;
+assign usb_data_o[17] = FIFO_D17;
+assign usb_data_o[16] = FIFO_D16;
+assign usb_data_o[15] = FIFO_D15;
+assign usb_data_o[14] = FIFO_D14;
+assign usb_data_o[13] = FIFO_D13;
+assign usb_data_o[12] = FIFO_D12;
+assign usb_data_o[11] = FIFO_D11;
+assign usb_data_o[10] = FIFO_D10;
+assign usb_data_o[9]  = FIFO_D9;
+assign usb_data_o[8]  = FIFO_D8;
+assign usb_data_o[7]  = FIFO_D7;
+assign usb_data_o[6]  = FIFO_D6;
+assign usb_data_o[5]  = FIFO_D5;
+assign usb_data_o[4]  = FIFO_D4;
+assign usb_data_o[3]  = FIFO_D3;
+assign usb_data_o[2]  = FIFO_D2;
+assign usb_data_o[1]  = FIFO_D1;
+assign usb_data_o[0]  = FIFO_D0;
+
+// Define USB to BluejayData Interface
+usb_to_bluejay_if_inst usb_to_bluejay_if(
+
+  // Control
+  .reset_i(reset_all_w),
+  // USB-Fifo Side
+  .clk_i(sys_clk),  //TODO: Fix our sysclk as this will be wrong
+  .data_i(usb_data_o),
+  .next_line_rdy_i(next_line_rdy_i_w),
+  .next_frame_rdy_i(next_frame_rdy_i_w),
+  .fifo_empty_i(RX_F),
+  .fifo_output_enable_o(FT_OE),
+  .get_next_word_o(RD_N),
+  .reset_o(RESET_N),
+  // Bluejay Data Interface
+  .clk_o(),  //TODO: Fix our sysclk as this will be wrong
+  .data_o(usb_data_o),
+  .next_line_rdy_o(next_line_rdy_o_w),
+  .next_frame_rdy_o(next_frame_rdy_o_w),
+  .fifo_empty_o(fifo_empty_i),
+  .get_next_word_i(get_next_word_o)
+	  
+ );
 
 
 
@@ -638,42 +780,42 @@ assign FT_WR    = 1'bz;
 assign FT_SIWU  = 1'bz;
 // assign FR_RXF   = 1'bz;
 // assign FT_TXE   = 1'bz;
-assign FIFO_BE3 = 1'bz;
-assign FIFO_BE2 = 1'bz;
-assign FIFO_BE1 = 1'bz;
-assign FIFO_BE0 = 1'bz;
-assign FIFO_D31 = 1'bz;
-assign FIFO_D30 = 1'bz;
-assign FIFO_D29 = 1'bz;
-assign FIFO_D28 = 1'bz;
-assign FIFO_D27 = 1'bz;
-assign FIFO_CLK = 1'bz;
-assign FIFO_D26 = 1'bz;
-assign FIFO_D25 = 1'bz;
-assign FIFO_D24 = 1'bz;
-assign FIFO_D23 = 1'bz;
-assign FIFO_D22 = 1'bz;
-assign FIFO_D21 = 1'bz;
-assign FIFO_D20 = 1'bz;
-assign FIFO_D19 = 1'bz;
-assign FIFO_D18 = 1'bz;
-assign FIFO_D17 = 1'bz;
-assign FIFO_D16 = 1'bz;
-assign FIFO_D15 = 1'bz;
-assign FIFO_D14 = 1'bz;
-assign FIFO_D13 = 1'bz;
-assign FIFO_D12 = 1'bz;
-assign FIFO_D11 = 1'bz;
-assign FIFO_D10 = 1'bz;
-assign FIFO_D9  = 1'bz;
-assign FIFO_D8  = 1'bz;
-assign FIFO_D7  = 1'bz;
-assign FIFO_D6  = 1'bz;
-assign FIFO_D5  = 1'bz;
-assign FIFO_D4  = 1'bz;
-assign FIFO_D3  = 1'bz;
-assign FIFO_D2  = 1'bz;
-assign FIFO_D1  = 1'bz;
+// assign FIFO_BE3 = 1'bz;
+// assign FIFO_BE2 = 1'bz;
+// assign FIFO_BE1 = 1'bz;
+// assign FIFO_BE0 = 1'bz;
+// assign FIFO_D31 = 1'bz;
+// assign FIFO_D30 = 1'bz;
+// assign FIFO_D29 = 1'bz;
+// assign FIFO_D28 = 1'bz;
+// assign FIFO_D27 = 1'bz;
+// assign FIFO_CLK = 1'bz;
+// assign FIFO_D26 = 1'bz;
+// assign FIFO_D25 = 1'bz;
+// assign FIFO_D24 = 1'bz;
+// assign FIFO_D23 = 1'bz;
+// assign FIFO_D22 = 1'bz;
+// assign FIFO_D21 = 1'bz;
+// assign FIFO_D20 = 1'bz;
+// assign FIFO_D19 = 1'bz;
+// assign FIFO_D18 = 1'bz;
+// assign FIFO_D17 = 1'bz;
+// assign FIFO_D16 = 1'bz;
+// assign FIFO_D15 = 1'bz;
+// assign FIFO_D14 = 1'bz;
+// assign FIFO_D13 = 1'bz;
+// assign FIFO_D12 = 1'bz;
+// assign FIFO_D11 = 1'bz;
+// assign FIFO_D10 = 1'bz;
+// assign FIFO_D9  = 1'bz;
+// assign FIFO_D8  = 1'bz;
+// assign FIFO_D7  = 1'bz;
+// assign FIFO_D6  = 1'bz;
+// assign FIFO_D5  = 1'bz;
+// assign FIFO_D4  = 1'bz;
+// assign FIFO_D3  = 1'bz;
+// assign FIFO_D2  = 1'bz;
+// assign FIFO_D1  = 1'bz;
 // assign FIFO_D0  = 1'bz;
 
 
