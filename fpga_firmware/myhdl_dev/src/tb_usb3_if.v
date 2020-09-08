9b4e6b3cba210eab9e933850f7453dd5ee8e6175
unknown <Kadds>
2020-09-08 17:34:39 +0100

    almost working but getting the terrible buffer switch in the last quarter of a line clock out problem.... grrrr

--- fpga_firmware/myhdl_dev/src/tb_usb3_if.v
+++ fpga_firmware/myhdl_dev/src/tb_usb3_if.v
@@ -12,7 +12,6 @@ wire write_to_dc32_fifo;
 wire [31:0] dc32_fifo_data_in;
 reg dc32_fifo_almost_full;
 reg dc32_fifo_empty;
-wire [3:0] STATE_DEBUG_B0;
 
 initial begin
     $from_myhdl(
@@ -29,8 +28,7 @@ initial begin
         FT_OE,
         FT_RD,
         write_to_dc32_fifo,
-        dc32_fifo_data_in,
-        STATE_DEBUG_B0
+        dc32_fifo_data_in
     );
 end
 
@@ -46,8 +44,7 @@ usb3_if dut(
     write_to_dc32_fifo,
     dc32_fifo_data_in,
     dc32_fifo_almost_full,
-    dc32_fifo_empty,
-    STATE_DEBUG_B0
+    dc32_fifo_empty
 );
 
 endmodule
